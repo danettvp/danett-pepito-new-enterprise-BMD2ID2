c9bc19c461d1a61ce559169074c235eb1e9c2762
danett <danett.pepito@mds.ac.nz>
2025-06-13 22:44:21 +1200

    Initial Commit
    Basic code for main pages

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.close-menu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('open');
+});
